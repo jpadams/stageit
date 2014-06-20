b1186a0949a2ea05086a52ae6c26e1046faf0429
Jeremy Adams <jeremy@puppetlabs.com>
2014-06-20 10:59:07 -0700

    fixed windows owner and perms

--- manifests/windows.pp
+++ manifests/windows.pp
@@ -2,19 +2,19 @@ class stageit::windows {
 
   file { 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective/agent/stageit.rb':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/stageit/stageit_win.rb',
+    owner  => Administrator,
+    group  => Administrators,
+    mode   => '0644',
     notify => Service['pe-mcollective']
   }
 
   file { 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective/agent/stageit.ddl':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
     source => 'puppet:///modules/stageit/stageit.ddl',
+    owner  => Administrator,
+    group  => Administrators,
+    mode   => '0644',
     notify => Service['pe-mcollective']
   }
 
