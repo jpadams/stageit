65114dee3eef041d5e6685d8750fb2868cc43543
Jeremy Adams <jeremy@puppetlabs.com>
2014-06-22 12:41:35 -0700

    Hopefully added 32 bit windows coverage

--- files/stageit_windows.rb
+++ files/stageit_windows.rb
@@ -14,7 +14,12 @@ module MCollective
         manifest = Base64.decode64(request[:manifest])
         file.write(manifest)
         file.close
-        puppet = 'C:/Progra~2/Puppet~1/Puppet~1/bin/puppet.bat'
+        case Facts["hardwaremodel"]
+        when "x64"
+          puppet = 'C:/Progra~2/Puppet~1/Puppet~1/bin/puppet.bat'
+        else # 32bit
+          puppet = 'C:/Progra~1/Puppet~1/Puppet~1/bin/puppet.bat'
+        end
         command = "cmd /c \"#{puppet} apply #{path} --detailed-exitcodes\""
         reply[:status] = run(command,:stdout => :out, :stderr => :err, :chomp => true)
         file.unlink
